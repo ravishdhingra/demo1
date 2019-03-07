857af9cda2a659c54c67e80e5fb3de45208c9c88
ravishdhingra <48288882+ravishdhingra@users.noreply.github.com>
2019-03-07 15:43:27 +0530

    Speech Updated to test

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
+//  var speech =
+//    req.body.result &&
+//    req.body.result.parameters &&
+//    req.body.result.parameters.echoText
+//     ? req.body.result.parameters.echoText
+//      : "Seems like some problem. Speak again.";
   var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+    req.body.result.parameters.echoText;
+
+  
   return res.json({
     speech: speech,
     displayText: speech,
