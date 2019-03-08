5f702d35e515dcf2518ab9db48f6a097aefa4fc8
ravishdhingra <48288882+ravishdhingra@users.noreply.github.com>
2019-03-08 15:56:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-     ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+//   var speech =
+//     req.body.result &&
+//     req.body.result.parameters &&
+//     req.body.result.parameters.echoText
+//      ? req.body.result.parameters.echoText
+//       : "Seems like some problem. Speak again.";
 
-
+var speech = "Hi there";
   
   return res.json({
     speech: speech,
